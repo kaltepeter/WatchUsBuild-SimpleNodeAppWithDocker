fdc4d89ae7826ae9b0935abb98728e7e7f2a6402
Kayla Altepeter <kayla@kaylaaltepeter.com>
2017-10-15 09:27:03 -0500

    part 2: postgres setup

--- node/src/server.js
+++ node/src/server.js
@@ -6,22 +6,22 @@ let bodyParser = require('body-parser');
 let ejs = require('ejs');
 let pg = require('pg');
 
-// let client = new pg.Client('postgres://postgres:blahblah@172.17.0.1:9000/postgres');
+let client = new pg.Client('postgres://postgres:blahblah@172.17.0.1:9000/postgres');
 
 let votes = {
   sandwiches: 0,
   tacos: 0
 };
 
-// client.connect(function (err) {
-//   if (err) throw err;
-//   client.query('SELECT number_of_votes FROM votes', function (err, result) {
-//     if (err) throw err;
+client.connect(function (err) {
+  if (err) throw err;
+  client.query('SELECT number_of_votes FROM votes', function (err, result) {
+    if (err) throw err;
 
-//     votes.sandwiches = result.rows[0].number_of_votes;
-//     votes.tacos = result.rows[1].number_of_votes;
-//   });
-// });
+    votes.sandwiches = result.rows[0].number_of_votes;
+    votes.tacos = result.rows[1].number_of_votes;
+  });
+});
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -38,14 +38,14 @@ app.post('/vote', urlencodedParser, function(req, res) {
   let vote = req.body.yourVote;
   if(vote === 'sandwiches') {
     votes.sandwiches = votes.sandwiches + 1;
-      // client.query('UPDATE votes SET number_of_votes=\'' + votes.sandwiches + '\' WHERE option_name=\'sandwiches\'', function (err, result) {
-      //   if (err) throw err;
-      // });
+      client.query('UPDATE votes SET number_of_votes=\'' + votes.sandwiches + '\' WHERE option_name=\'sandwiches\'', function (err, result) {
+        if (err) throw err;
+      });
   } else if(vote === 'tacos') {
     votes.tacos = votes.tacos + 1;
-      // client.query('UPDATE votes SET number_of_votes=\'' + votes.tacos + '\' WHERE option_name=\'tacos\'', function (err, result) {
-      //   if (err) throw err;
-      // });
+      client.query('UPDATE votes SET number_of_votes=\'' + votes.tacos + '\' WHERE option_name=\'tacos\'', function (err, result) {
+        if (err) throw err;
+      });
   } else {
     console.log('Something went wrong: vote contains ' + vote);
   }
